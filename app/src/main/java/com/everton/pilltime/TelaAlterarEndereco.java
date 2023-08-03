54ef3b5dd43489252da0684d4c06bf40969076ae
Everton <evertoncabral.dev1@gmail.com>
2023-08-02 23:39:07 -0300

    XML Tela endereço, botão buscar CEP

---
+++ app/src/main/java/com/everton/pilltime/TelaAlterarEndereco.java
@@ -0,0 +1,13 @@
+package com.everton.pilltime;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaAlterarEndereco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_alterar_endereco);
+    }
+}
