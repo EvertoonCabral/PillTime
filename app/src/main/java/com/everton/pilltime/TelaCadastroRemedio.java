95a1f5114bf52187ed2f72563225d679ad6eedd4
Everton Cabral <96452540+EvertoonCabral@users.noreply.github.com>
2023-10-07 16:27:54 -0300

    refazendo tela principal
    retrabalho.....

--- app/src/main/java/com/everton/pilltime/TelaCadastroRemedio.java
+++ app/src/main/java/com/everton/pilltime/TelaCadastroRemedio.java
@@ -8,25 +8,14 @@ import android.widget.Spinner;
 
 public class TelaCadastroRemedio extends AppCompatActivity {
 
-    String[] formasFarmaceuticas = {
-            "Comprimido",
-            "Cápsula",
-            "Xarope",
-            "Pomada",
-            "Outro"
-    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro_remedio);
 
-        Spinner spinnerFormaFarmaceutica = findViewById(R.id.spinnerFormaFarmaceutica);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, formasFarmaceuticas);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerFormaFarmaceutica.setAdapter(adapter);
 
     }
 }
--- app/src/main/java/com/everton/pilltime/TelaPrincipal.java
+++ app/src/main/java/com/everton/pilltime/TelaPrincipal.java
@@ -1,8 +1,12 @@
 package com.everton.pilltime;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class TelaPrincipal extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.btn_remedio:
+                        Intent intent = new Intent(TelaPrincipal.this, TelaCadastroRemedio.class);
+                        startActivity(intent);
+                        return true;
+
+                    case R.id.btn_relatorio:
+                        Intent intent2 = new Intent(TelaPrincipal.this, TelaRelatorios.class);
+                        startActivity(intent2);
+                        return true;
+
+                    // Descomente e atualize o código abaixo quando tiver a atividade correspondente
+                    // case R.id.btnCadastroIdoso:
+                    //    Intent intent3 = new Intent(TelaPrincipal.this, NomeDaAtividade.class);
+                    //    startActivity(intent3);
+                    //    return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 }
