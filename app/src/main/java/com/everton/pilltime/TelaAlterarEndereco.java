b2822b19cc90fb6ea9a36090b60b2409bb961d67
Everton Cabral <96452540+EvertoonCabral@users.noreply.github.com>
2023-10-09 00:02:48 -0300

    Validação
    Adicionado validaões na tela de cadastro de Idoso

--- app/src/main/java/com/everton/pilltime/TelaAlterarEndereco.java
+++ app/src/main/java/com/everton/pilltime/TelaAlterarEndereco.java
@@ -2,12 +2,120 @@ package com.everton.pilltime;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TelaAlterarEndereco extends AppCompatActivity {
+    EditText edCepUsuario, edRuaUsuario, edBairrolUsuario, edCidadeUsuario, edEstadoUsuario, edNumLogradouroUsuario, edComplementoUsuario;
+    Button btnBuscarCep, btnSalvar, btnLimpar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_alterar_endereco);
+
+            iniciarComponentes();
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validarEndereco()) {
+
+
+
+                    Toast.makeText(TelaAlterarEndereco.this, "Dados salvos com sucesso!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                limparCampos();
+            }
+        });
+
+
+
+
+
+
     }
+
+
+
+
+
+
+    private boolean validarEndereco() {
+        String cep = edCepUsuario.getText().toString().trim();
+        String rua = edRuaUsuario.getText().toString().trim();
+        String bairro = edBairrolUsuario.getText().toString().trim();
+        String cidade = edCidadeUsuario.getText().toString().trim();
+        String estado = edEstadoUsuario.getText().toString().trim();
+        String numero = edNumLogradouroUsuario.getText().toString().trim();
+
+        if (cep.length() != 8 || !cep.matches("[0-9]+")) {
+            Toast.makeText(this, "CEP inválido. Deve ter 8 números.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (rua.isEmpty() || rua.length() > 100) {
+            Toast.makeText(this, "Rua inválida. Deve ter entre 1 e 100 caracteres.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (bairro.isEmpty() || bairro.length() > 50) {
+            Toast.makeText(this, "Bairro inválido. Deve ter entre 1 e 50 caracteres.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (cidade.isEmpty() || cidade.length() > 50) {
+            Toast.makeText(this, "Cidade inválida. Deve ter entre 1 e 50 caracteres.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (estado.isEmpty() || (estado.length() != 2 && estado.length() > 20)) {
+            Toast.makeText(this, "Estado inválido. Deve ter 2 caracteres (abreviação) ou até 20 caracteres (nome completo).", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (numero.isEmpty() || numero.length() > 10 || !numero.matches("[0-9]+")) {
+            Toast.makeText(this, "Número inválido. Deve ter até 10 números.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void iniciarComponentes(){
+
+        edCepUsuario = findViewById(R.id.edCepUsuario);
+        edRuaUsuario = findViewById(R.id.edRuaUsuario);
+        edBairrolUsuario = findViewById(R.id.edBairrolUsuario);
+        edCidadeUsuario = findViewById(R.id.edCidadeUsuario);
+        edEstadoUsuario = findViewById(R.id.edEstadoUsuario);
+        edNumLogradouroUsuario = findViewById(R.id.edNumLogradouroUsuario);
+        edComplementoUsuario = findViewById(R.id.edComplementoUsuario);
+        btnBuscarCep = findViewById(R.id.btnBuscarCep);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnLimpar = findViewById(R.id.btnLimpar);
+
+    }
+
+    private void limparCampos() {
+        edCepUsuario.setText("");
+        edRuaUsuario.setText("");
+        edBairrolUsuario.setText("");
+        edCidadeUsuario.setText("");
+        edEstadoUsuario.setText("");
+        edNumLogradouroUsuario.setText("");
+        edComplementoUsuario.setText("");
+    }
+
+
+
+
 }
--- app/src/main/java/com/everton/pilltime/TelaCadastroIdoso.java
+++ app/src/main/java/com/everton/pilltime/TelaCadastroIdoso.java
@@ -1,14 +1,139 @@
 package com.everton.pilltime;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.everton.pilltime.databinding.ActivityTelaCadastroIdosoBinding;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class TelaCadastroIdoso extends AppCompatActivity {
+
+    private ActivityTelaCadastroIdosoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_cadastro_idoso);
+        binding = ActivityTelaCadastroIdosoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Agora, você pode acessar todos os componentes diretamente através do objeto binding
+        // Por exemplo: binding.edNomeCadastroIdoso, binding.btSalvarCadastro, etc.
+
+        binding.btSalvarCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                validarCampos();
+
+            }
+        });
+
+        binding.btLimparDadosUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                limparDadosUsuario();
+            }
+        });
+
+        binding.btLimparDadosEndereco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                limparDadosEndereco();
+            }
+        });
     }
+
+
+    private boolean validarCampos() {
+        String nome = binding.edNomeCadastroIdoso.getText().toString().trim();
+        String senha = binding.edSenhaCadastroIdoso.getText().toString().trim();
+        String cpf = binding.edCPFCadastroIdoso.getText().toString().trim();
+        String dataNascimento = binding.edDataNascCadastroIdoso.getText().toString().trim();
+        String estado = binding.edEstadoCadastroCuidador.getText().toString().trim();
+        String cidade = binding.edCidadeCadastroIdoso.getText().toString().trim();
+
+        if (nome.length() < 3 || nome.length() > 50) {
+            binding.edNomeCadastroIdoso.setError("O nome deve ter entre 2 e 50 caracteres.");
+            Toast.makeText(this, "O nome deve ter entre 2 e 50 caracteres.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (senha.length() < 6) {
+            binding.edSenhaCadastroIdoso.setError("A senha deve ter mais de 6 caracteres.");
+            Toast.makeText(this, "A senha deve ter mais de 6 caracteres.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (cpf.length() != 11) {
+            binding.edCPFCadastroIdoso.setError("O CPF deve ter 11 caracteres.");
+            Toast.makeText(this, "O CPF deve ter 11 caracteres.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!validarDataNascimento(dataNascimento)) {
+            // A mensagem de erro específica será exibida dentro da função validarDataNascimento
+            return false;
+        }
+
+        if (cidade.isEmpty()) {
+            binding.edCidadeCadastroIdoso.setError("Cidade não pode estar vazia.");
+            Toast.makeText(this, "Cidade não pode estar vazia.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (estado.length() != 2 || !estado.matches("[A-Z]{2}")) {
+            binding.edEstadoCadastroCuidador.setError("Estado deve ser uma sigla válida (ex: PR)!");
+            Toast.makeText(this, "Estado deve ser uma sigla válida (ex: PR)!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        Toast.makeText(this, "Dados validados com sucesso!", Toast.LENGTH_SHORT).show();
+        return true;
+
+    }
+
+    private boolean validarDataNascimento(String dataNascimento) {
+        SimpleDateFormat sdf;
+        if (dataNascimento.contains("/")) {
+            sdf = new SimpleDateFormat("dd/MM/yyyy");
+        } else {
+            sdf = new SimpleDateFormat("ddMMyyyy");
+        }
+        sdf.setLenient(false);
+        try {
+            Date dataNasc = sdf.parse(dataNascimento);
+
+        } catch (ParseException e) {
+            binding.edDataNascCadastroIdoso.setError("Formato de data inválido. Use dd/MM/yyyy ou ddmmyyyy.");
+            Toast.makeText(this, "Formato de data inválido. Use dd/MM/yyyy ", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+
+
+    private void limparDadosUsuario() {
+        binding.edNomeCadastroIdoso.setText("");
+        binding.edEmailCadastroIdoso.setText("");
+        binding.edSenhaCadastroIdoso.setText("");
+        binding.edCPFCadastroIdoso.setText("");
+        binding.edTelefoneCadastroIdoso.setText("");
+        binding.edDataNascCadastroIdoso.setText("");
+    }
+
+    private void limparDadosEndereco() {
+        binding.edCidadeCadastroIdoso.setText("");
+        binding.edEstadoCadastroCuidador.setText("");
+        binding.edBairroCadastroIdoso.setText("");
+        binding.edRuaCadastroIdoso.setText("");
+        binding.edNumeroCadastroCuidador.setText("");
+    }
+
 }
