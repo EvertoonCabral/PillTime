4c9f54a8b68c089ba6298836b2d5438ea558b4a9
Everton Cabral <evertoncabral.dev1@gmail.com>
2023-11-19 23:40:51 -0300

    ajustado tamanho de elementos
    Ajustando tamanho da lista da tela principal

--- app/src/main/java/com/everton/pilltime/api/Retrofit.java
+++ app/src/main/java/com/everton/pilltime/api/Retrofit.java
@@ -59,6 +59,6 @@ public class Retrofit {
     public static ApiCuidador GET_CUIDADOR(){ return retrofit.create(ApiCuidador.class);}
     public static ApiCuidador POST_REMEDIO_TO_CUIDADOR(){return  retrofit.create(ApiCuidador.class);}
     public static ApiCuidador GET_ALL_REMEDIO_CUIDADOR (){return  retrofit.create(ApiCuidador.class);}
-    
+
 
 }
