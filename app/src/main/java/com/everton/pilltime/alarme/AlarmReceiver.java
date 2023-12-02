e14c31bd00bdcd07359f71e61433f888328c3332
Everton Cabral <evertoncabral.dev1@gmail.com>
2023-12-02 18:00:08 -0300

    Ajustes
    Ajustes

--- app/src/main/java/com/everton/pilltime/alarme/AlarmReceiver.java
+++ app/src/main/java/com/everton/pilltime/alarme/AlarmReceiver.java
@@ -11,18 +11,14 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d("AlarmReceiver", "Alarme recebido");
 
-        // Recebe o título e a descrição do alarme do Intent
         String titulo = intent.getStringExtra("TITULO");
         String descricao = intent.getStringExtra("DESCRICAO");
 
-        // Cria um Intent para abrir a AlarmDetailsActivity
         Intent alarmIntent = new Intent(context, AlarmDetailsActivity.class);
 
-        // Passa o título e a descrição para a AlarmDetailsActivity
         alarmIntent.putExtra("TITULO", titulo);
         alarmIntent.putExtra("DESCRICAO", descricao);
 
-        // Inicia a AlarmDetailsActivity
         alarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(alarmIntent);
     }
